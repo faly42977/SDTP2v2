a63bbf53bd7a8d04f3770efa4f8d74f43c8ce0bc
Unknown <f.aly@campus.fct.unl.pt>
2018-05-24 23:06:17 +0100

    codigo do prof  v2

---
+++ src/sys/storage/rest/namenode/NamenodeServer.java
@@ -0,0 +1,38 @@
+package sys.storage.rest.namenode;
+
+import static utils.Log.Log;
+
+import java.net.URI;
+import java.util.logging.Level;
+
+import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import api.storage.Namenode;
+import discovery.Discover;
+import utils.IP;
+
+
+public class NamenodeServer {
+	public static final String NAMENODE = "Namenode";
+
+	public static final int NAMENODE_PORT = 7777;
+
+	public static void main(String[] args) throws Exception {
+		System.setProperty("java.net.preferIPv4Stack", "true");
+
+		Log.setLevel( Level.FINER );
+
+		String ip = IP.hostAddress();
+		String serverURI = String.format("http://%s:%s/", ip, NAMENODE_PORT);
+		
+		ResourceConfig config = new ResourceConfig();
+		config.register( new NamenodeResources() );
+		
+		JdkHttpServerFactory.createHttpServer( URI.create(serverURI.replace(ip, "0.0.0.0")), config);
+
+		Log.fine(String.format("Namenode Server ready @ %s%s\n",  serverURI, Namenode.PATH.substring(1)));
+		
+		Discover.me(NAMENODE, serverURI);
+	}
+}
